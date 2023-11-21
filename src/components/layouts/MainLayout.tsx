87591583a6333a2d3ba6bc000387967b05ec3d31
Cottonwood-moa <blog_geon@naver.com>
2023-11-21 17:43:35 +0900

    refactor: storage 관련 변수명 명시적 변경

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -5,15 +5,15 @@ interface MainLayoutProps {
   children: React.ReactNode;
 }
 export default function MainLayout({ children }: MainLayoutProps) {
-  const [_value, setValue] = useLocalStorageWithSync('theme');
+  const [theme, setTheme] = useLocalStorageWithSync('theme');
 
   const setThemeFn = (newTheme: string) => {
-    setValue(newTheme);
+    setTheme(newTheme);
   };
   return (
     <>
-      <MainHeader setTheme={setThemeFn} />
-      <div className="p-4">{children}</div>
+      <MainHeader theme={theme} setTheme={setThemeFn} />
+      <div className="h-full overflow-y-auto p-4">{children}</div>
     </>
   );
 }
