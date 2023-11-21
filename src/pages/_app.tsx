0f2bcb9de9ce26378c1f5e087e5bff6b4c9b8ebf
Cottonwood-moa <blog_geon@naver.com>
2023-11-21 17:44:20 +0900

    fix: font 관련 누락 import 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import useLocalStorageWithSync from '@/hooks/useLocalStorageWithSync';
 import '@/styles/globals.scss';
+import '@fontsource/gugi';
+import '@fontsource/noto-sans-kr';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [value, _setValue] = useLocalStorageWithSync('theme');
-  useEffect(() => {}, [value]);
   return (
     <RecoilRoot>
       <div data-theme={value}>
