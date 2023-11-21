70182e18309d8b275ea846aafb45c078c82d7762
Cottonwood-moa <blog_geon@naver.com>
2023-11-21 17:42:32 +0900

    feat: header component > theme 관련 기능 추가

--- src/components/headers/MainHeader.tsx
+++ src/components/headers/MainHeader.tsx
@@ -1,7 +1,12 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { capitalize } from 'lodash';
+import Svg from '../common/Svg';
+
 interface MainHeaderProps {
+  theme: string;
   setTheme: (theme: string) => void;
 }
-export default function MainHeader({ setTheme }: MainHeaderProps) {
+export default function MainHeader({ theme, setTheme }: MainHeaderProps) {
   const daisyUiThemeList = [
     'light',
     'dark',
@@ -37,33 +42,85 @@ export default function MainHeader({ setTheme }: MainHeaderProps) {
     'sunset',
   ];
   /**
-   * @param {string} theme
+   * @param {string} newTheme
    * @description theme 변경 이벤트
    */
-  const onClickSelectTheme = (theme: string) => {
-    setTheme(theme);
+  const onClickSelectTheme = (newTheme: string) => {
+    setTheme(newTheme);
   };
 
   return (
-    <label className="swap swap-rotate">
-      <input type="checkbox" className="theme-controller" value="synthwave" />
-      <div className="dropdown">
-        <label tabIndex={0} className="btn m-1">
-          Click
+    <div className="flex items-center justify-between bg-warning-content pb-1 pl-3 pr-3 pt-1">
+      {/* 왼쪽 컨텐츠 */}
+      <div />
+      {/* 오른쪽 컨텐츠 */}
+      {/* Theme 변경 아이콘 */}
+      <div className="flex items-center">
+        {theme === 'light' && (
+          <AnimatePresence>
+            <motion.div
+              className="mr-4 flex h-7 w-7 cursor-pointer items-center justify-center"
+              animate={{ opacity: 1, rotate: 360 }}
+              onClick={() => onClickSelectTheme('dark')}
+            >
+              {/* icon-sun */}
+              <Svg type="icon-sun" />
+            </motion.div>
+          </AnimatePresence>
+        )}
+        {theme === 'dark' && (
+          <AnimatePresence>
+            <motion.div
+              className="mr-4 h-7 w-7 cursor-pointer items-center justify-center"
+              animate={{ opacity: 1, rotate: 360 }}
+              onClick={() => onClickSelectTheme('light')}
+            >
+              {/* icon-moon */}
+              <Svg type="icon-moon" />
+            </motion.div>
+          </AnimatePresence>
+        )}
+        {theme !== 'dark' && theme !== 'light' && (
+          <AnimatePresence>
+            <motion.div
+              className="mr-4 h-7 w-7 cursor-pointer items-center justify-center"
+              animate={{ opacity: 1, rotate: 360 }}
+              onClick={() => onClickSelectTheme('light')}
+            >
+              {/* icon-point-brush */}
+              <Svg type="icon-point-brush" />
+            </motion.div>
+          </AnimatePresence>
+        )}
+        {/* Theme 변경 Dropdown */}
+        <label className="swap swap-rotate">
+          <input
+            type="checkbox"
+            className="theme-controller"
+            value="synthwave"
+          />
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-sm m-1 font-gugi">
+              {capitalize(theme)}
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content z-[1] block max-h-96 w-52 overflow-y-auto rounded-box bg-base-100 p-2 shadow"
+            >
+              {daisyUiThemeList.map((item) => (
+                <li key={item}>
+                  <button
+                    type="button"
+                    onClick={() => onClickSelectTheme(item)}
+                  >
+                    {capitalize(item)}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </label>
-        <ul
-          tabIndex={0}
-          className="menu dropdown-content z-[1] block max-h-96 w-52 overflow-y-auto rounded-box bg-base-100 p-2 shadow"
-        >
-          {daisyUiThemeList.map((theme) => (
-            <li key={theme}>
-              <button type="button" onClick={() => onClickSelectTheme(theme)}>
-                {theme}
-              </button>
-            </li>
-          ))}
-        </ul>
       </div>
-    </label>
+    </div>
   );
 }
