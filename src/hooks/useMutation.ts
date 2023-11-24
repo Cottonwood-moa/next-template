888854328ad9ab18954861fb340c0d3257c776b2
Cottonwood-moa <blog_geon@naver.com>
2023-11-24 12:01:20 +0900

    feat: useMutation 개선

--- src/hooks/useMutation.ts
+++ src/hooks/useMutation.ts
@@ -1,29 +1,47 @@
 import { useState } from 'react';
-import { AxiosRequestConfig } from 'axios';
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-interface UseMutationState<T> {
+interface UseMutationState<T, F> {
   isLoading: boolean;
   data?: T;
-  error?: object;
+  error?: AxiosResponse<F>;
 }
 
-export default function useMutation<T>(
+export default function useMutation<T, F>(
   url: string,
-  fetcher: (url: string, body: unknown, config?: AxiosRequestConfig) => Promise<T>
+  fetcher: (
+    url: string,
+    body: unknown,
+    config?: AxiosRequestConfig,
+  ) => Promise<AxiosResponse>,
 ) {
-  const [state, setState] = useState<UseMutationState<T>>({
+  const [state, setState] = useState<UseMutationState<T, F>>({
     isLoading: false,
     data: undefined,
     error: undefined,
   });
 
-  async function mutation(body: unknown, config: AxiosRequestConfig = {}){
-    console.log('하이킥 url', url, fetcher, body);
-    setState((prev) => ({...prev, isLoading: true}));
-    const res = await fetcher(url, body, config);
-    setState((prev) => ({...prev, data: res, isLoading: false}));
-    console.log('하이킥 useMutation res', res);
+  async function mutation(body: unknown, config: AxiosRequestConfig = {}) {
+    setState((prev) => ({ ...prev, isLoading: true }));
+    try {
+      const res = await fetcher(url, body, config);
+      const axiosResTypeAssertion = res as AxiosResponse<T>;
+      setState((prev) => ({ ...prev, data: axiosResTypeAssertion.data }));
+    } catch (error) {
+      const axiosErrorTypeAssertion = error as AxiosError<F>;
+      setState((prev) => ({
+        ...prev,
+        error: axiosErrorTypeAssertion.response,
+      }));
+    } finally {
+      setState((prev) => ({ ...prev, loading: false }));
+    }
   }
 
-  return {data: state.data, isLoading: state.isLoading, mutation};
+  return {
+    data: state.data,
+    error: state.error,
+    isLoading: state.isLoading,
+    mutation,
+  };
 }
