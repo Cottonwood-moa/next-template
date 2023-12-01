3f7e7c967e09b0b9128e6f97d42236a56496858b
Cottonwood-moa <blog_geon@naver.com>
2023-12-01 11:42:18 +0900

    style: PostCard UI 세팅

--- src/components/common/PostCard.tsx
+++ src/components/common/PostCard.tsx
@@ -19,16 +19,17 @@ export default function PostCard({ id, title, body }: PostCardProps) {
         stiffness: 260,
         damping: 20,
       }}
-      className="h-[380px] w-full min-w-[300px] rounded-md bg-primary-content p-2"
+      className="h-[380px] w-[96%] min-w-[300px] rounded-md bg-primary-content"
     >
       <figure>
         <img
-          className="h-[240px] w-full rounded-md object-cover"
-          src="https://www.creativefabrica.com/wp-content/uploads/2023/09/20/Youtube-Thumbnail-Design-Template-Graphics-79693258-1.jpg"
+          className="h-[200px] w-full max-w-[450px] rounded-md object-cover"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
           alt="post"
         />
       </figure>
-      <span>{id}</span>
+      <div className="h-[120px] bg-slate-600">body</div>
+      <div className="h-[calc(100%-320px)] bg-slate-800">footer</div>
     </motion.div>
   );
 }
